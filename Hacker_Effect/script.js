de17453f42d5bf4a776e8f4905d3664c6497ce89
Ayush-tmg <ayushtamang1995@gmail.com>
2023-08-10 16:54:01 +0545

    git commit --amend -m "changes on css and js"

--- Hacker_Effect/script.js
+++ Hacker_Effect/script.js
@@ -1,22 +1,28 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-document.querySelector("h1").onmouseover = event => {
-  let iterations = 0;
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
   
-  const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("")
-    .map((letters, index) => {
-        if(index < iterations){
-            return event.target.dataset.value[index];
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
         }
+      
         return letters[Math.floor(Math.random() * 26)]
-    })
-    .join("");
+      })
+      .join("");
     
-    if(iterations >= event.target.dataset.value.length) {
-        clearInterval(interval);
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
     }
     
-    iterations += 1 / 3;
+    iteration += 1 / 3;
   }, 30);
 }
